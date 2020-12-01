4b19d10732933fd28af4d404ddada424c7d3696d
achyutsun <achyutsun@users.noreply.github.com>
2020-12-01 15:05:59 -0700

    Create pythonmongorest.py

---
+++ pythonmongorest.py
@@ -0,0 +1,35 @@
+# mongorest.py
+from flask import Flask
+from flask_restplus import Api, Resource, fields
+# from werkzeug.utils import cached_property
+from flask import jsonify
+from flask import request
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+api = Api(app)
+
+app.config['MONGO_DBName'] = 'stars'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/restdb'
+mongo = PyMongo(app)
+
+@app.route('/star', methods=['GET'])
+def get_all_stars():
+    star = mongo.db.stars
+    output = []
+    for s in star.find():
+        output.append({'name': s['name'], 'distance': s['distance']})
+    return jsonify({'result': output})
+
+@app.route('/star', methods=['POST'])
+def add_star():
+    star = mongo.db.stars
+    name = request.json['name']
+    distance = request.json['distance']
+    star_id = star.insert({'name':name, 'distance': distance})
+    new_star = star.find_one({'_id': star_id })
+    output = {'name': new_star['name'], 'distance': new_star['distance']}
+    return jsonify({'result' : output})
+
+if __name__ == "__main__":
+    app.run(debug=True)
